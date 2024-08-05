b3b662f88e88df32a9bec2dd69f145c43aac65cb
Ubuntu <ubuntu@ip-172-31-35-109.ec2.internal>
2024-08-05 23:38:33 +0000

    scripts for suspicious activity log

--- analyzing.sh
+++ analyzing.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-sed -n '/error/p; /Failed/p; /Unauthorized/p' /var/log/auth_log.log >> /home/ubuntu/bash_analyzing/suspicious.log
-echo "executed" 
+#identify the keyword for suspicious activities: "error", "Unauthorized", "Failed"
+#use sed to get the whole entry and write into a daily file. This will be over write daily
+sed -n '/error/p; /Failed/p; /Unauthorized/p' /var/log/auth_log.log > /home/ubuntu/bash_analyzing/suspicious_daily.log
+
+#keep all daily logs into a new file that contains old suspicious activities
+echo "$(date)" >> /home/ubuntu/bash_analyzing/suspicious_activity.log
+cat /home/ubuntu/bash_analyzing/suspicious_daily.log >> /home/ubuntu/bash_analyzing/suspicious_activity.log
---
+++ cron_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#the path to analyzing script to catch suspicious activities
+SCRIPT_PATH="~/bash_analyzing/analyzing.sh"
+
+#list all previous cron jobs to a file
+crontab -l > CRONTAB_NEW
+
+#create a cronjob if cronjob does not exist
+#run daily. everyday at 00:00
+CRON_JOB="0 0 * * * $SCRIPT_PATH"
+
+#check the temp file for cron jobs
+grep -F "$CRON_JOB" ./CRONTAB_NEW > /dev/null
+
+if [ $? -eq 0 ]
+then
+	echo "This job exists."
+else
+	echo "$CRON_JOB" >> CRONTAB_NEW #append new job
+	crontab CRONTAB_NEW #execute
+fi
+#remove temp file
+rm CRONTAB_NEW
+
+
+
